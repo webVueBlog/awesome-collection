6b414038dcbff61ab06d57cb1dd049b4fb62494f
webVueBlog <2979986590@qq.com>
2022-01-27 09:38:06 +0800

    docs

--- getPageScrollLeft.js
+++
@@ -1,9 +0,0 @@
-/**
- * @description 获取页面scrollLeft
- * @author 未知
- */
-
-function getPageScrollLeft(){
-    var a = document;
-    return a.documentElement.scrollLeft || a.body.scrollLeft;
-}
--- getPageScrollTop.js
+++
@@ -1,9 +0,0 @@
-/**
- * @description 获取页面scrollTop
- * @author 未知
- */
-
-function getPageScrollTop(){
-    var a = document;
-    return a.documentElement.scrollTop || a.body.scrollTop;
-}
